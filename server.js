f61626f40c10df23de493d86e2e798edd329f3a3
chris <104324755+cjurgens17@users.noreply.github.com>
2023-07-23 12:50:36 -0400

    addidng welcome get

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(bodyParser.json());
 
 const MY_DOMAIN = 'https://coffeebrothers-7ec5c7fdf171.herokuapp.com';
 
+app.get('/', (req, res) => {
+  res.send('<h1>Coffee Brothers Server!</h1>');
+});
+
 app.post('/create-checkout-session', async (req,res) => {
 try{
   console.log(req.body);
